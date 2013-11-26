c3ebfe3d14fef1bcc8dc104bdf8aed782b922038
Richard Collette <rcollette@yahoo.com>
2013-11-26 15:18:11 -0500

    Route error removed

--- AngularLearning.Web/Scripts/app/appRoutes.js
+++ AngularLearning.Web/Scripts/app/appRoutes.js
@@ -5,7 +5,7 @@ app.config([
     '$routeProvider',
     '$locationProvider',
     function ($routeProvider, $locationProvider) {
-        $routeProvider.when('#/partialPageView1', { template: 'page 1', controller: null }).when('#/partialPageView1', { template: 'page 2', controller: null }).otherwise({ redirectTo: 'HTMLPage1' });
+        $routeProvider.when('partialPageView1', { template: 'partials/partial1.htm', controller: null }).when('partialPageView2', { template: 'partials/partial2.htm', controller: null }).otherwise({ redirectTo: 'HTMLPage1.htm' });
         $locationProvider.html5Mode(true);
     }
 ]);
--- AngularLearning.Web/Scripts/app/appRoutes.ts
+++ AngularLearning.Web/Scripts/app/appRoutes.ts
@@ -3,8 +3,8 @@
 "use strict";
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.
-        when('#/partialPageView1', { template: 'page 1', controller: null }).
-        when('#/partialPageView1', { template: 'page 2', controller: null }).
-        otherwise({ redirectTo: 'HTMLPage1' });
+        when('partialPageView1', { template: 'partials/partial1.htm', controller: null }).
+        when('partialPageView2', { template: 'partials/partial2.htm', controller: null }).
+        otherwise({ redirectTo: 'HTMLPage1.htm' });
     $locationProvider.html5Mode(true);
 }]);
